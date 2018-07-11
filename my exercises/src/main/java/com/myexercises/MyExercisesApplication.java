8706f9b1d7213dfc0bfbefc61b99d19e67425141
Rafal Bujak <bujak.rafal@yahoo.com>
2018-07-11 18:37:31 +0200

    first commit

---
+++ my exercises/src/main/java/com/myexercises/MyExercisesApplication.java
@@ -0,0 +1,12 @@
+package com.myexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExercisesApplication.class, args);
+	}
+}
---
+++ my exercises/src/test/java/com/myexercises/MyExercisesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myexercises;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyExercisesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
